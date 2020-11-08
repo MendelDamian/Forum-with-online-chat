ae5bbe65ea0dc6060e36362b0f4d4f6a4be203c5
Damian Mendel <mendeldamian01@gmail.com>
2020-11-08 20:53:02 +0100

    test

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<!-- <?php require_once 'includes/baseTopSite.inc.php'; ?>
-<?php require_once 'includes/parts/chat.part.php'; ?>
-<?php require_once 'includes/baseBottomSite.inc.php'; ?>
+<!-- <?php //require_once 'includes/baseTopSite.inc.php'; ?>
+<?php //require_once 'includes/parts/chat.part.php'; ?>
+<?php //require_once 'includes/baseBottomSite.inc.php'; ?>
  -->
  <?php
 
