4e8a847614a6fdcc8aff7dd9a1ea37023e0698d8
Damian Mendel <mendeldamian01@gmail.com>
2020-11-08 20:40:06 +0100

    test

--- classes/Database.class.php
+++ classes/Database.class.php
@@ -3,10 +3,10 @@
 class Database {
 
   protected function connect() {
-    $host = $_ENV['CLEARDB_DATABASE_HOST'] ?: 'localhost';
-    $user = $_ENV['CLEARDB_DATABASE_USERNAME'] ?: 'root';
-    $pwd = $_ENV['CLEARDB_DATABASE_PASSWORD'] ?: '';
-    $dbName = $_ENV['CLEARDB_DATABASE_DATABASE'] ?: 'Forum';
+    $host = $_ENV['CLEARDB_DATABASE_HOST'];
+    $user = $_ENV['CLEARDB_DATABASE_USERNAME'];
+    $pwd = $_ENV['CLEARDB_DATABASE_PASSWORD'];
+    $dbName = $_ENV['CLEARDB_DATABASE_DATABASE'];
 
     $dsn = 'mysql:host=' . $host . ';dbname=' . $dbName;
     $pdo = new PDO($dsn, $user, $pwd);
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php require_once 'includes/baseTopSite.inc.php'; ?>
-<?php require_once 'includes/parts/chat.part.php'; var_dump($_ENV['CLEARDB_DATABASE_PASSWORD']); ?>
+<?php require_once 'includes/parts/chat.part.php'; ?>
 <?php require_once 'includes/baseBottomSite.inc.php'; ?>
