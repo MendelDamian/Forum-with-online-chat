9eced767b687ae89a1c5795d3ffcf3431f015183
Damian Mendel <mendeldamian01@gmail.com>
2020-11-08 21:14:35 +0100

    test

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
-<?php require_once 'includes/baseTopSite.inc.php'; ?>
-<?php require_once 'includes/parts/chat.part.php'; ?>
-<?php require_once 'includes/baseBottomSite.inc.php'; ?>
+<?php //require_once 'includes/baseTopSite.inc.php'; ?>
+<?php //require_once 'includes/parts/chat.part.php'; ?>
+<?php //require_once 'includes/baseBottomSite.inc.php'; 
+
+    $host = getenv('CLEARDB_DATABASE_HOST') ?: 'localhost';
+    $user = getenv('CLEARDB_DATABASE_USERNAME') ?: 'root';
+    $pwd = getenv('CLEARDB_DATABASE_PASSWORD') ?: '';
+    $dbName = getenv('CLEARDB_DATABASE_DATABASE') ?: 'Forum';
+    try {
+      $dsn = 'mysql:host=' . $host . ';dbname=' . $dbName . ';port=3306';
+      $pdo = new PDO($dsn, $user, $pwd);
+      $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+      die('Connection failed: ' . $e->getMessage());
+    }
+    return $pdo;
+
+?>
