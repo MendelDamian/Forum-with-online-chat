9efce2c3166250046ccd506ff230d9622e8fbf2c
Damian Mendel <mendeldamian01@gmail.com>
2020-11-08 20:48:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php require_once 'includes/baseTopSite.inc.php'; ?>
+<!-- <?php require_once 'includes/baseTopSite.inc.php'; ?>
 <?php require_once 'includes/parts/chat.part.php'; ?>
 <?php require_once 'includes/baseBottomSite.inc.php'; ?>
+ -->
+ <?php
+
+echo $_ENV['CLEARDB_DATABASE_USERNAME'];
+
+$host = $_ENV['CLEARDB_DATABASE_HOST'];
+$user = $_ENV['CLEARDB_DATABASE_USERNAME'];
+$pwd = $_ENV['CLEARDB_DATABASE_PASSWORD'];
+$dbName = $_ENV['CLEARDB_DATABASE_DATABASE'];
+
+try {
+  $dbh = new PDO($dsn, $user, $password);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo 'Connection failed: ' . $e->getMessage();
+}
+
+ ?>
