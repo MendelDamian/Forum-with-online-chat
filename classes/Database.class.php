98085d14389c4dc159a65347b24a603512483bdb
Damian Mendel <mendeldamian01@gmail.com>
2020-11-08 21:11:58 +0100

    test

--- classes/Database.class.php
+++ classes/Database.class.php
@@ -7,10 +7,13 @@ class Database {
     $user = getenv('CLEARDB_DATABASE_USERNAME') ?: 'root';
     $pwd = getenv('CLEARDB_DATABASE_PASSWORD') ?: '';
     $dbName = getenv('CLEARDB_DATABASE_DATABASE') ?: 'Forum';
-
-    $dsn = 'mysql:host=' . $host . ';dbname=' . $dbName . ';port=3306';
-    $pdo = new PDO($dsn, $user, $pwd);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    try {
+      $dsn = 'mysql:host=' . $host . ';dbname=' . $dbName . ';port=3306';
+      $pdo = new PDO($dsn, $user, $pwd);
+      $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+      die('Connection failed: ' . $e->getMessage());
+    }
     return $pdo;
   }
 
