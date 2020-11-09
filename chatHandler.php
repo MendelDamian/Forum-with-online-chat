4419fcff077cd5b1352623b497862f5a1a07bd48
Damian Mendel <mendeldamian01@gmail.com>
2020-11-09 21:56:20 +0100

    test

--- chatHandler.php
+++ chatHandler.php
@@ -2,8 +2,10 @@
 if (empty($_SERVER['HTTP_X_REQUESTED_WITH']) === false && 
     strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest' &&
     isset($_POST['method']) === true && empty($_POST['method']) === false) {
+  var_dump($_SERVER, $_POST);
   var_dump('weszlo chatHandel');
   require_once 'includes/autoLoaderClass.inc.php';
+  var_dump('autoloader przeszlo chatHandler');
 
   $chatContr = new chatController();
   $chatView = new ChatView();
